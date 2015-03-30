ec3f318a50168eaef2a535512895437445078cac
Masayoshi Sekimura <sekimura@gmail.com>
2015-03-30 16:30:22 -0700

    first

---
+++ README.md
@@ -0,0 +1,20 @@
+# google chrome extension to show the total fee of airbnb trip
+
+I was finding it very frustrating that the search results page shows 
+the 'per night' room fee but doesn't include the various service fees
+until I've clicked through individual room pages and want to book.
+
+Until now.
+
+With this google chrome extension, you will be able to see the total fee
+of the trip including cleaning fee, service fee and etc.
+
+## Install
+
+Download the crx binary from [Releases](https://github.com/sekimura/airbnb-total-price/releases).
+To install the extension, open the chrome://extensions page and drag and drop the `.crx` file from the Downloads.
+
+## License
+
+MIT © [Masayoshi Sekimura](http://sekimura.org)
+
---
+++ myscript.js
@@ -0,0 +1,26 @@
+function patch(){
+  var urlBase = "/rooms/ajax_refresh_subtotal?utf8=%E2%9C%93&";
+  var token = $('#csrf-token-meta-tag').attr('content');
+  var data = $('.map-search').data('bootstrap-data');
+
+  $('.listing').each(function(i, el) {
+    var $el = $(el);
+    var id = $el.data('id');
+    var qdict = {};
+    qdict.authenticity_token = token;
+    qdict.checkin = data.checkin;
+    qdict.checkout = data.checkout;
+    qdict.number_of_guests = data.guests;
+    qdict.hosting_id = id;
+
+    var url = urlBase + $.param(qdict);
+    $.get(url, function(data) {
+      var thePrice = data.total_price_with_fees_and_tax_native;
+      $el.find('.price-amount').text(thePrice);
+    });
+  });
+}
+
+var script = document.createElement('script');
+script.appendChild(document.createTextNode('('+patch+')();'));
+document.body.appendChild(script);
